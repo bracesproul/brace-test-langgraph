45edf208db1e83854cbab4f61d006bcb35745f7f
Isaac Francisco <78627776+isahers1@users.noreply.github.com>
2024-11-04 17:46:59 -0800

    Update agent.py

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -1,6 +1,6 @@
 from typing import TypedDict, Literal
 
-from langgraph.graph import StateGraph, END
+from langgraph.graph import StateGraph, END, START
 from my_agent.utils.nodes import call_model, should_continue, tool_node
 from my_agent.utils.state import AgentState
 from langchain_openai import ChatOpenAI
